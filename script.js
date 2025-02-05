5752c2daa33852f4e8e391eca9c962e2985aeadc
andrewwerd <60647745+andrewwerd@users.noreply.github.com>
2025-02-05 15:47:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const fireworksContainer = document.querySelector('.fireworks');
+const yesBtn = document.getElementById('yes-btn');
+const loveBtn = document.getElementById('love-btn');
+const responseText = document.getElementById('response');
+const questionBlock = document.getElementById('question');
 
 function createFirework() {
     const randomNumber = Math.floor(Math.random() * 10) + 1;
@@ -6,7 +10,7 @@ function createFirework() {
     for (let i = 0; i <= randomNumber; i++) {
         let firework = document.createElement('div');
         firework.classList.add('firework');
-        
+
         let x = Math.random() * window.innerWidth;
         let y = Math.random() * window.innerHeight;
 
@@ -21,5 +25,20 @@ function createFirework() {
     }
 
 }
-
 setInterval(createFirework, 100);
+
+// Анимация кнопок
+yesBtn.addEventListener('click', () => {
+    showResponse()
+});
+
+loveBtn.addEventListener('click', () => {
+    showResponse()
+});
+
+// Запускаем дополнительные фейерверки при нажатии кнопки
+function showResponse() {
+    responseText.style.opacity = 1;
+    responseText.style.display = 'block';
+    questionBlock.style.display = 'none'
+}
